61057e0e7f0a5cd0d26cf52372e87785a6489139
andicuevasp <andicuevasp@gmail.com>
2021-09-08 19:39:36 +0200

    Updates

---
+++ precache-manifest.b463c27f7a09d0a2cf928df88a307da8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6258a3b8ff2ee0aca69a",
+    "url": "/sticky-note-ui/static/css/main.1f2a5394.chunk.css"
+  },
+  {
+    "revision": "6258a3b8ff2ee0aca69a",
+    "url": "/sticky-note-ui/static/js/main.44bf9a97.chunk.js"
+  },
+  {
+    "revision": "627f8107a2bdc968ccf8",
+    "url": "/sticky-note-ui/static/js/runtime~main.5f23ea22.js"
+  },
+  {
+    "revision": "59fa5faa0d43ca05c280",
+    "url": "/sticky-note-ui/static/js/2.da757658.chunk.js"
+  },
+  {
+    "revision": "5dc4ec233d0f02a650161757a1d70c84",
+    "url": "/sticky-note-ui/index.html"
+  }
+];
